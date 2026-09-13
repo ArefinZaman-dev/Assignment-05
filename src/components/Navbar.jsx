4340cdb4657980791e41371fc9182a58c176df37
Shaikh Arefin Zaman <shaikhzaman953@gmail.com>
2026-09-13 22:53:59 +0600

    add mobile navbar and responsive layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,78 @@
 const Navbar = () => {
 
+
     return (
 
         <nav className="navbar">
 
-            <div className="logo">
-                DevStack
+
+            <div className="mobile-menu">
+
+                ☰
+
             </div>
 
 
+
+            <div className="logo">
+
+                Dev Stack
+
+            </div>
+
+
+
             <div className="nav-links">
+
 
                 <a href="#">
                     Home
                 </a>
 
+
                 <a href="#">
                     Technologies
                 </a>
+
 
                 <a href="#">
                     Projects
                 </a>
 
+
                 <a href="#">
                     About
                 </a>
+
 
                 <a href="#">
                     Contact
                 </a>
 
+
             </div>
+
 
 
             <div className="nav-buttons">
 
+
                 <button className="signin-btn">
+
                     Sign In
+
                 </button>
 
 
                 <button className="signup-btn">
+
                     Sign Up
+
                 </button>
 
+
             </div>
+
 
 
         </nav>
@@ -53,5 +80,6 @@ const Navbar = () => {
     )
 
 }
+
 
 export default Navbar;
