bf6d2259ae76d2a43c06dc4298e9b1fa0f1d0f5c
Shaikh Arefin Zaman <shaikhzaman953@gmail.com>
2026-09-13 18:21:07 +0600

    Add technology stack selection functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import Hero from "./components/Hero";
 import TechnologySection from "./components/TechnologySection";
 import Footer from "./components/Footer";
 
-
 const App = () => {
 
     const [technologies, setTechnologies] = useState([]);
 
     const [loading, setLoading] = useState(true);
 
+    const [stack, setStack] = useState([]);
     useEffect(() => {
 
         fetch("/src/data/technologies.json")
@@ -27,24 +27,35 @@ const App = () => {
         })
 
     }, [])
+    const addToStack = (technology) => {
+        const alreadyAdded = stack.find(
+            item => item.id === technology.id
+        );
 
+        if(alreadyAdded){
+
+            alert("Already Added");
+
+            return;
+
+        }
+        setStack([...stack, technology]);
+
+    }
     return (
 
         <>
 
             <Navbar />
-
             <Hero />
-
             {
-                loading ?  <h2> Loading... </h2> :  <TechnologySection technologies={technologies} />
+                loading ? <h2> Loading...</h2> : <TechnologySection technologies={technologies} addToStack={addToStack} stack={stack} />
 
             }
-
             <Footer />
+
         </>
 
     )
 }
-
 export default App;
--- src/components/TechnologySection.jsx
+++ src/components/TechnologySection.jsx
@@ -1,20 +1,30 @@
 import TechnologyCard from "./TechnologyCard";
 
-const TechnologySection = ({ technologies }) => {
+
+const TechnologySection = ({ technologies, addToStack, stack }) => {
+
 
     return (
+
         <section>
 
             <h2>
                 Explore Technologies
             </h2>
 
+
+            <h3>
+                {stack.length} Technology Selected
+            </h3>
+
+
             <div>
 
                 {
                     technologies.map((technology)=>(
-                        
-                        <TechnologyCard  key={technology.id} technology={technology} />
+
+                        <TechnologyCard key={technology.id} technology={technology} addToStack={addToStack} isAdded={stack.some(item => item.id === technology.id)}
+                        />
 
                     ))
                 }
@@ -24,6 +34,8 @@ const TechnologySection = ({ technologies }) => {
         </section>
 
     )
+
 }
+
 
 export default TechnologySection;
