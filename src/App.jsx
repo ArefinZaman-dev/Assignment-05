2606338354d81019252d35d56a46a5fefa4e4f1e
Shaikh Arefin Zaman <shaikhzaman953@gmail.com>
2026-09-13 18:04:57 +0600

    Add technology data and load JSON with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
@@ -5,17 +6,39 @@ import Footer from "./components/Footer";
 
 const App = () => {
 
+    const [technologies, setTechnologies] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+
+        fetch("/src/data/technologies.json")
+        .then(res => res.json())
+        .then(data => {
+
+            setTechnologies(data);
+            setLoading(false);
+
+        })
+
+    }, [])
+
+
     return (
 
         <>
 
             <Navbar />
+
             <Hero />
+            {
+                loading ?         
+                <h2>Loading...</h2>  : <h2>{technologies.length} Technologies Loaded</h2>
+
+            }
+
             <Footer />
 
         </>
 
     )
 }
-
 export default App;
